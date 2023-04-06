deba919c55208d55fad1d68aa977dbddc84defb0
uzakotim <timauzakov@gmail.com>
2023-04-06 13:40:02 +0500

    Playground function

---
+++ C++/Playground/executeTest.h
@@ -0,0 +1,10 @@
+// Function to write commands to produce tests
+// Write your tests here
+/* ----------------------------------------------------- */
+#include "handleTest.h"
+void execute_tests()
+{
+	// handleTest(testNumber,int input0, reference)
+	handleTest(1,1, 0);
+}
+/* ----------------------------------------------------- */
--- C++/Playground/libraries.h
+++ C++/Playground/libraries.h
@@ -1,14 +1,14 @@
 //Include all necessary libraries here
 /*----------------------------------------------------------*/
-// #include <stdio.h>
-// #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <iostream>
 #include <algorithm>
-// #include <math.h>
-// #include <vector>
-// #include <utility>
-// #include <string_view>
+#include <math.h>
+#include <vector>
+#include <utility>
+#include <string_view>
 /*----------------------------------------------------------*/
 
 
